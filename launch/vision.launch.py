c205c2f41225672fcb1b01137bd4e8f2666e2ed4
spyalexs <spyalexs>
2022-07-31 17:05:41 -0400

    updated launch node to call vision

--- launch/vision.launch.py
+++ launch/vision.launch.py
@@ -1,12 +1,18 @@
 import launch
 import os
 import launch_ros.actions
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument 
 from ament_index_python.packages import get_package_share_directory
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import PushRosNamespace
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+
+robot_name = "tempest"
 
 def generate_launch_description():
     riptide_vision_share_dir = get_package_share_directory('riptide_vision2')
+
+    yoloV5_launch_file_path = os.path.join(get_package_share_directory('yolov5_ros'), "launch", "yolov5s_simple.launch.py")
+
 
     riptide_vision = launch_ros.actions.Node(
         package="riptide_vision2", executable="vision",
@@ -22,7 +28,19 @@ def generate_launch_description():
             arguments=["-0.242", "0.283", "-0.066", "0", "0", "0", "tempest/base_link", "tempest/stereo/left_optical"]
     )
 
+    
+
     return launch.LaunchDescription([
-        riptide_vision,
-        static_transform
+        # riptide_vision,
+        # static_transform
+
+        DeclareLaunchArgument('robot', default_value=robot_name, description="Name of the vehicle"),
+
+        #push the the namespace
+        PushRosNamespace(robot_name),
+
+        #launch yolo
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(yoloV5_launch_file_path),
+        ),
     ])
